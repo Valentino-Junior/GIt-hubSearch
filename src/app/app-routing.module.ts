783432df8ffba75bf553bb0da7bd193b0baf8a09
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-18 01:42:08 +0300

    Added path componentss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { CHomeComponent } from './c-home/c-home.component';
+import { CRepositoryComponent } from './c-repository/c-repository.component';
+import { CUserComponent } from './c-user/c-user.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+
 
 
 const routes: Routes = [
+  {path:'', component:CHomeComponent},
+  {path:'user', component:CUserComponent},
+  {path:'repo', component:CRepositoryComponent},
+  {path:'**', component:NotFoundPageComponent},
+
   
   
 ];
