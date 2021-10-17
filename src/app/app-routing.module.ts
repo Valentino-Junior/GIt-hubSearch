9655f98e0fef01cdef4e7ffdce6607756519f717
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-18 00:44:48 +0300

    Added class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
