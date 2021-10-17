f6186b8c4e5fdb22bf5c67bb8ce7340b6fd34991
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-17 14:35:33 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
