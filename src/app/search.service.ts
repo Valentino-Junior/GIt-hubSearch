ffdd6d72b951a1e685dcbe4296bdf21619676b92
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-19 09:36:28 +0300

    Renamed the private properties

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -4,6 +4,7 @@ import { Repository } from './repository';
 import { HttpClient } from '@angular/common/http';
 
 
+
 Users: User;
 Repo: Repository;
 
@@ -12,21 +13,22 @@ Repo: Repository;
 })
 export class SearchService {
 
-  private username = "valentine-ochieng";
-  private client_id = "87c09bd30ffdfb0c197f";
-  private client_secret = "0d47157b2e665bfc71d6f6ebdbe7785872c1002a";
+  private username = "string";
+  private clientId = "87c09bd30ffdfb0c197f";
+  private clientSecret = "0d47157b2e665bfc71d6f6ebdbe7785872c1002a";
 
   constructor(private http: HttpClient) {
     console.log("Github service started");
+    this.username = "valentine-ochieng";
   }
   getUser() {
     return this.http.get(
       "https://api.github.com/users/" +
       this.username +
       "?client_id=" +
-      this.client_id +
+      this.clientId +
       "&client_secret=" +
-      this.client_secret
+      this.clientSecret
     );
   }
   getRepos() {
@@ -35,9 +37,9 @@ export class SearchService {
       this.username +
       "/repos" +
       "?client_id=" +
-      this.client_id +
+      this.clientId +
       "&client_secret=" +
-      this.client_secret
+      this.clientSecret
     );
   }
   newUsername(username: any) {
