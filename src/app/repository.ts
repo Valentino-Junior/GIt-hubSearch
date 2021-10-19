6d2a404052d9c022bfbc43e7bcee1441ffbb97e3
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-19 07:59:33 +0300

    Updated the repository class

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,5 +1,7 @@
 export class Repository {
     public name!: string;
+    public  description!: string;
+    public  created_at!: string;
     public html_url!: string;
-   public  description!: string;
+   
 }
