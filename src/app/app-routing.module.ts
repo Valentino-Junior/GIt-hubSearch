66a6f97ba5dc828510fa17b42320e42cf27d5530
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-19 14:47:54 +0300

    Added not found page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { CHomeComponent } from './c-home/c-home.component';
 import { CRepositoryComponent } from './c-repository/c-repository.component';
 import { CUserComponent } from './c-user/c-user.component';
-// import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 
 
 
 const routes: Routes = [
-  {path:'GIt-hubSearch', component:CHomeComponent},
-  {path:'user', component:CUserComponent},
-  {path:'repo', component:CRepositoryComponent},
-  {path:'', redirectTo: '/GIt-hubSearch', pathMatch: 'full' },
-  // {path:'**', component:NotFoundPageComponent},
+  { path: 'GIt-hubSearch', component: CHomeComponent },
+  { path: 'user', component: CUserComponent },
+  { path: 'repo', component: CRepositoryComponent },
+  { path: '', redirectTo: '/GIt-hubSearch', pathMatch: 'full' },
+  { path: '**', component: NotFoundPageComponent },
 
-  
-  
+
+
 ];
 
 @NgModule({
