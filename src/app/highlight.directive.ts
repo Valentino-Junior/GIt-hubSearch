62bc1003465c8f0cdb78c03b217c48e894cdf0d1
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-10-19 08:17:47 +0300

    Added higlight color to the repos

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -7,7 +7,7 @@ export class HighlightDirective {
   constructor(private el: ElementRef) {}
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('green-yellow');
+    this.highlight('cyan');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
